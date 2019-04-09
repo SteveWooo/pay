a887a32ee4c8b6238c6d3cf617f4eb6d69e45a9c
461437874@qq.com <461437874@qq.com>
2019-04-10 00:00:09 +0800

    add db

--- server/routers/public/pay/getSign.js
+++ server/routers/public/pay/getSign.js
@@ -14,7 +14,7 @@ module.exports = async (req, res, next)=>{
 	//懒得透传了。。免得依赖的服务器出问题
 	var tempQuery = {};
 	for(var i in req.query){
-		if(i != 'email' && i != 'name' && i != message){
+		if(i != 'email' && i != 'name' && i != 'message'){
 			tempQuery[i] = req.query[i];
 		}
 	}
